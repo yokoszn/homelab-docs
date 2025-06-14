e77eb393afacd84a9618682febb8e6d408ff0874
Clay Townsend <dev@clayatownsend.com>
2025-06-14 15:51:02 +1000

    Quartz sync: Jun 14, 2025, 3:51 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "wizardryatwork.clayatownsend.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
